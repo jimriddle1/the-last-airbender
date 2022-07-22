66e4e2e63447aabf4f7b9ed9fae7d877faf8b130
Jim Riddle <jimriddle0@gmail.com>
2022-07-22 10:53:01 -0500

    wrote feature test and got it to click the button

---
+++ spec/features/character_search_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Character Search Page' do
+  it 'Shows first 25 characters of fire nation' do
+
+    visit "/"
+    # save_and_open_page
+    select 'Fire Nation', from: 'nation'
+
+    click_button 'Search For Members'
+
+    expect(current_path).to eq("/search")
+
+    expect(page).to have_content('Total People: 97')
+    expect(page).to have_content('Name: Chan (Fire Nation admiral)')
+    expect(page).to have_content('Allies: Ozai')
+    expect(page).to have_content('Enemies: Earth Kingdom')
+    expect(page).to have_content('Affiliation: Fire Nation Army')
+
+
+    expect(page).to_not have_content('Name: Great Sage')
+  end
+end
