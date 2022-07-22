443cb3465d9e309eae6405ee33ac8991a704484c
Jim Riddle <jimriddle0@gmail.com>
2022-07-22 11:21:32 -0500

    wrote tests for allies and enemies methods

--- spec/poros/character_spec.rb
+++ spec/poros/character_spec.rb
@@ -33,4 +33,46 @@ RSpec.describe Character do
     expect(character.photoUrl).to eq(character_data[:photoUrl])
   end
 
+  it 'gives me the enemies and allies using poro methods' do
+    character_data =
+    {
+        "_id": "5cf5679a915ecad153ab6906",
+        "allies": [
+          "Ty Lee", "Jim Riddle"
+        ],
+        "enemies": [
+          "Appa", "Nick Jacobs"
+        ],
+        "photoUrl": "https://vignette.wikia.nocookie.net/avatar/images/a/a5/Circus_master.png/revision/latest?cb=20130706153819",
+        "name": "Circus master",
+        "affiliation": "Fire Nation circus"
+      }
+
+
+    character = Character.new(character_data)
+
+    expect(character.print_allies).to eq("Ty Lee, Jim Riddle")
+    expect(character.print_enemies).to eq("Appa, Nick Jacobs")
+  end
+
+  it 'gives me the enemies and allies using poro methods - return none if blank array' do
+    character_data =
+    {
+        "_id": "5cf5679a915ecad153ab6906",
+        "allies": [
+        ],
+        "enemies": [
+        ],
+        "photoUrl": "https://vignette.wikia.nocookie.net/avatar/images/a/a5/Circus_master.png/revision/latest?cb=20130706153819",
+        "name": "Circus master",
+        "affiliation": "Fire Nation circus"
+      }
+
+
+    character = Character.new(character_data)
+
+    expect(character.print_allies).to eq("None")
+    expect(character.print_enemies).to eq("None")
+  end
+
 end
